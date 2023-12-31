7aceb5fad043ed29fec687ef260267578a0df222
Kevin Donahue <nonnontrivial@gmail.com>
2023-12-31 17:11:54 -0500

    use datetime to parse astro time hour

--- prediction/utils.py
+++ prediction/utils.py
@@ -1,9 +1,8 @@
+from datetime import datetime
+
 from astropy.time import Time
 
 
 def get_astro_time_hour(astro_time: Time) -> int:
-    """Get the hour value within the time object."""
-    iso: str = astro_time.iso
-    second_segment = iso.split(" ")[1]
-    mins_idx = second_segment.index(":")
-    return int(second_segment[:mins_idx])
+    dt = datetime.strptime(astro_time.iso, "%Y-%m-%d %H:%M:%S.%f")
+    return dt.hour
