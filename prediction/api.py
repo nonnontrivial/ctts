256ed36e7366aad81dbd40312960be0d9350fb9b
Kevin Donahue <nonnontrivial@gmail.com>
2024-01-12 17:56:59 -0500

    use correct typing on handler

--- prediction/api.py
+++ prediction/api.py
@@ -5,7 +5,6 @@ from .prediction import Prediction, get_model_prediction_for_astro_twilight_type
 from .constants import API_PREFIX
 
 app = FastAPI()
-allowed_twilight_types = {"next","previous","nearest"}
 
 def create_response_from_prediction(prediction: Prediction) -> t.Dict:
     y = round(float(prediction.y.item()), 4)
@@ -15,14 +14,12 @@ def create_response_from_prediction(prediction: Prediction) -> t.Dict:
     }
 
 @app.get(f"{API_PREFIX}/prediction")
-async def get_prediction(lat, lon, astro_twilight_type: str = "nearest"):
+async def get_prediction(lat, lon, astro_twilight_type: t.Literal["next","nearest","previous"]):
     """Get model's sky brightness prediction at the latitude and longitude.
 
     Optionally pass query param astro_twilight_type to control which astronomical
     twilight should be used relative to the current time.
     """
-    if astro_twilight_type not in allowed_twilight_types:
-        raise HTTPException(status_code=400,detail=f"`astro_twilight_type` must be in {allowed_twilight_types}")
     try:
         lat, lon = float(lat), float(lon)
         prediction = await get_model_prediction_for_astro_twilight_type(lat, lon, astro_twilight_type)
