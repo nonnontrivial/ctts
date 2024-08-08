53c7bbadf9ead848f3815500b3ed92bcacf8d401
Kevin Donahue <nonnontrivial@gmail.com>
2024-08-07 22:31:55 -0400

    fix pg volume name

--- pp/pp/prediction_message.py
+++ pp/pp/prediction_message.py
@@ -6,10 +6,8 @@ class BrightnessMessage:
     uuid: str
     lat: float
     lon: float
-    # id of the h3 cell
     h3_id: str
     utc_iso: str
     utc_ns: int
-    # magnitudes per square arc second estimated by the model
     mpsas: float
     model_version: str
