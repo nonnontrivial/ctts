ceeda3dbe8c4b9020f9612e63d0a7097cead2542
Kevin Donahue <nonnontrivial@gmail.com>
2025-02-02 16:01:53 -0500

    fix open meteo script

--- update-open-meteo-data.sh
+++ update-open-meteo-data.sh
@@ -2,11 +2,15 @@
 
 volume_name="open-meteo-data"
 
+sync_open_meteo_data() {
+    docker run -it --rm -v open-meteo-data:/app/data ghcr.io/open-meteo/open-meteo sync ecmwf_ifs04 cloud_cover,temperature_2m --past-days 3
+}
+
 if docker volume ls -q | grep -q "^${volume_name}$"; then
     echo "volume $volume_name exists; updating volume"
-    docker run -it --rm -v open-meteo-data:/app/data ghcr.io/open-meteo/open-meteo sync ecmwf_ifs04 cloud_cover,temperature_2m --past-days 3
+    sync_open_meteo_data
 else
-    cmd="docker volume create --name open-meteo-data"
-    echo "$volume_name does not exist; create it by running '$cmd'"
-    exit 1
+    echo "$volume_name does not exist; creating it"
+    docker volume create --name open-meteo-data
+    sync_open_meteo_data
 fi
