b1fa4c9786bad15f7e1f65f616811db010710d0e
Kevin Donahue <nonnontrivial@gmail.com>
2025-03-01 16:05:50 -0500

    use uv in dockerfiles

--- scripts/unpack.py
+++ scripts/unpack.py
@@ -15,10 +15,8 @@ def main() -> None:
     """extract the contents of the gan zip into ./data/brightness_data"""
     data_dir_path = list(Path.cwd().parent.rglob("brightness_data"))[0]
     assert data_dir_path.exists(), "data dir path does not exist!"
-
     zip_path = Path.cwd().parent / "gan.zip"
     assert zip_path.exists(), "zip does not exist!"
-
     log.info(f"unpacking {zip_path} to {data_dir_path}")
     shutil.unpack_archive(zip_path, data_dir_path, "zip")
 
