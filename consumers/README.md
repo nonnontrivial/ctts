fafddd0c2c8bd61f6eab3d3141f49ebe9f309ee6
Kevin Donahue <nonnontrivial@gmail.com>
2025-03-15 08:54:53 -0400

    add map generation to consumer example

--- consumers/README.md
+++ consumers/README.md
@@ -1,1 +1,12 @@
 # consumers
+
+## `store_in_sqlite.py`
+
+> n.b. assumes main stack is running (with `docker compose up`)
+
+- stores each snapshot in a SQLite database (`brightness.db`)
+- generates folium maps for each snapshot (with brightness values mapped to hex color values)
+
+```sh
+uv run store_in_sqlite.py
+```
--- consumers/store_in_sqlite.py
+++ consumers/store_in_sqlite.py
@@ -2,6 +2,8 @@
 # requires-python = ">=3.12"
 # dependencies = [
 #     "aio-pika",
+#     "folium>=0.19.5",
+#     "h3>=4.2.2",
 # ]
 # ///
 
@@ -10,6 +12,8 @@ import json
 import time
 import asyncio
 import aio_pika
+import folium
+import h3
 
 snapshot_queue = "brightness.snapshot"
 broker_url = "amqp://guest:guest@localhost/"
@@ -29,13 +33,48 @@ CREATE TABLE IF NOT EXISTS brightness (
 conn.commit()
 
 
-def insert_records(records: list[tuple]) -> None:
+def insert_cell_brightness_records(records: list[tuple]) -> None:
     cursor.executemany(
         """INSERT INTO brightness (cell, timestamp, brightness) VALUES (?, ?, ?)""",
         records,
     )
     conn.commit()
-    print(f"inserted {len(records)} records to {db}")
+    print(f"inserted {len(records)} records to brightness table in {db}")
+
+
+def store_snapshot(snapshot: dict[str, float]) -> None:
+    insert_cell_brightness_records(
+        [(x, int(time.time()), y) for x, y in snapshot.items()]
+    )
+
+
+def generate_map(snapshot: dict[str, float]) -> None:
+    def cell_brightness_to_hex_color(
+        value: float, *, lower_bound=16.0, upper_bound=22.0
+    ):
+        normalized = (upper_bound - value) / (upper_bound - lower_bound)
+        r = int(255 * (1 - normalized))
+        g = int(255 * (1 - normalized))
+        b = int(255 * (1 - normalized))
+        return f"#{r:02x}{g:02x}{b:02x}"
+
+    # TODO get centroid
+    cell = next(iter(snapshot.keys()))
+    m = folium.Map(location=list(h3.cell_to_latlng(cell)), zoom_start=10)
+    min_brightness = min(snapshot.values())
+    max_brightness = max(snapshot.values())
+    for cell, brightness in snapshot.items():
+        boundary = h3.cell_to_boundary(cell)
+        folium.Polygon(
+            boundary,
+            color="#000000",
+            fill=True,
+            fill_color=cell_brightness_to_hex_color(
+                brightness, lower_bound=min_brightness, upper_bound=max_brightness
+            ),
+            fill_opacity=0.5,
+        ).add_to(m)
+    m.save(f"map-{int(time.time())}.html")
 
 
 async def main() -> None:
@@ -46,13 +85,14 @@ async def main() -> None:
         async with queue.iterator() as queue_iter:
             async for message in queue_iter:
                 async with message.process():
-                    data = json.loads(message.body.decode()).get(
+                    snapshot: dict | None = json.loads(message.body.decode()).get(
                         "inferred_brightnesses", None
                     )
-                    if data is not None:
-                        insert_records(
-                            [(x, int(time.time()), y) for x, y in data.items()]
-                        )
+                    if snapshot is not None:
+                        store_snapshot(snapshot)
+                        generate_map(snapshot)
+                    else:
+                        print("no snapshot found")
     except aio_pika.exceptions.AMQPError as e:
         print("failed to start consumer; are the containers running?")
     finally:
